d20849da6c0b421237ba097993301f3fbcced151
Edyta <edyta.bosacka@gmail.com>
2021-08-28 19:42:55 +0200

    Adding books by category dashboard

--- BookLibrary_Client/pages/Books.js
+++ BookLibrary_Client/pages/Books.js
@@ -1,34 +1,89 @@
 import React, {useState} from "react";
 import "./Books.css";
 import { Dropdown } from 'semantic-ui-react';
+import Select from 'react-select';
 
-function Books() {
+class Books extends React.Component {
 	
-	const categoriesList = 
+	state = {
+        categories: [],
+		books: []
+    }
+	
+	componentDidMount() {
 		fetch('http://localhost:8080/BookLibraryManagement/api/category', {
 			method: 'GET',
 			headers: { 'Content-Type': 'application/json'					}
 		})
 		.then((response) => response.json())
 		.then((data => {
-			//const categoriesList = data.map((category) => ({
-			//	key: category,
-			//	text: category,
-			//	value: category,	
-			//}))	
-			//return categoriesList;
-			console.log(data);
+			const categoriesList = data.map((category) => ({
+				value: category,
+				label: category,	
+			}))	
+			
+			this.setState({ categories: categoriesList })
+			//console.log(this.state.books);
 		}))
 		.catch((error) => {
 			console.error(error);
 		})
+		
+	}
+	
+	handleChange(e){
+		console.log(e);
+		fetch('http://localhost:8080/BookLibraryManagement/api/category/' + e.value + '/books', {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json'					}
+		})
+		.then((response) => response.json())
+		.then((data => {
+			const booksByCategory = data.map((book) => (
+			{
+				title: book.title,
+				subtitle: book.subtitle,
+				authors : (book.authors != undefined ? book.authors.join(', ') : "" ) ,
+				publisher: book.publisher,
+				publishedDate: book.publishedDate,
+				imageLink: book.imageLinks.thumbnail,
+			}))
+			
+			this.setState({ books: booksByCategory });
+			console.log(this.state.books);
+		}))
+		
+	}
+  
+  
+	
+	render(){
+		
 	return (
 		<div id="books">
 			<h2> Books by Category </h2>
-			<Dropdown placeholder='State' search selection options={categoriesList} />
+			<Select options={this.state.categories} onChange={this.handleChange.bind(this)} />
+			
+			<div className = "grid">
+				{this.state.books.map((book) => {
+					return (
+						<div  className="bookCard">
+							<img className="bookImage" src={book.imageLink} alt="No image" />
+							<h3> {book.title} </h3>
+							<h5> {book.subtitle} </h5>
+							{book.authors ? <h4> Authors: {book.authors} </h4> : null}
+							{book.publisher ? <h4> Publisher: {book.publisher} </h4> : null}
+							{book.publishedDate ? <h4> Publication date: {book.publishedDate} </h4> : null}
+							
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	
+	
   );
+	}
 }
 
 export default Books;
