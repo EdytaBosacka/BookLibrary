f139c5fbc44ee374fb50919937b8cc119e8add1f
Edyta <edyta.bosacka@gmail.com>
2021-09-01 21:14:53 +0200

    Changing dropdown style

--- BookLibrary_Client/pages/Books.js
+++ BookLibrary_Client/pages/Books.js
@@ -59,10 +59,63 @@ class Books extends React.Component {
 	
 	render(){
 		
+		const customStyles = {
+		
+			option: (provided, state) => ({
+				...provided,
+				borderBottom: '1px solid #dfdfdf',
+				backgroundColor: state.isSelected ? 'rgba(255,231,220,0.5)' : 'white',
+				color: '#000000',
+				padding: 20,
+				width: 400,
+			}),
+			control: (provided) => ({
+				...provided,// none of react-select's styles are passed to <Control />
+				width: 400,
+				border: '2px solid #dfdfdf',
+				position: 'relative',
+				boxShadow: 'none',
+					'&:hover': {
+					border: '2px solid #ffe7dc',
+				}
+			}),
+			indicatorSeparator: () => {},
+			menu: (provided) => ({
+				...provided,
+				width: 400	 
+			}),
+			menuList: (base) => ({
+				...base,
+				"::-webkit-scrollbar": {
+					width: "8px"
+				},
+				"::-webkit-scrollbar-track": {
+     background: "#f4f4f4"
+   },
+   "::-webkit-scrollbar-thumb": {
+     background: "rgba(255,231,220,1)",
+	 "border-radius": "20px"
+   },
+   "::-webkit-scrollbar-thumb:hover": {
+     background: "rgba(255,231,220,1)"
+   }
+	}),
+
+  
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...provided, opacity, transition };
+  }
+  
+		};
+		
 	return (
 		<div id="books">
 			<h2> Books by Category </h2>
-			<Select options={this.state.categories} onChange={this.handleChange.bind(this)} />
+			
+			<Select styles={customStyles} options={this.state.categories} onChange={this.handleChange.bind(this)} />
 			
 			<div className = "grid">
 				{this.state.books.map((book) => {
