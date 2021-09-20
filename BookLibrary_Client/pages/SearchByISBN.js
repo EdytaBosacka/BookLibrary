04885da5ed593efb5042eeb56e1ccd9cd233c07c
Edyta <edyta.bosacka@gmail.com>
2021-09-20 21:02:38 +0200

    Displying info content for book searched by isbn

--- BookLibrary_Client/pages/SearchByISBN.js
+++ BookLibrary_Client/pages/SearchByISBN.js
@@ -8,7 +8,8 @@ import "./SearchByISBN.css";
 class SearchByISBN extends React.Component {
 	
 	state = {
-		book: {}
+		book: {},
+		error: ""
     }
 	componentDidMount() {
 		
@@ -21,10 +22,13 @@ class SearchByISBN extends React.Component {
 		})
 		.then((response) => response.json())
 		.then((data => {
-			this.setState({ book: data });
+			this.setState({ book: data, error:"" });
 			console.log(this.state.book);
 		}))
-		
+		.catch(error => {
+			this.setState({ error: "No results found.", 
+							book: {}});
+    });
 	}
 
 
@@ -35,8 +39,22 @@ class SearchByISBN extends React.Component {
 		<div id="isbn">
 			<h2> Search book by ISBN </h2>
 			<div className="searchByISBN">
-				<SearchField placeholder="Select..."				onSubmit={this.onSearch.bind(this)}/>
+				<SearchField placeholder="Search..." onSearchClick={this.onSearch.bind(this)}/>
 			</div>
+			{this.state.error ? <h3 className="error"> {this.state.error} </h3> : null}
+			{this.state.book.industryIdentifiers ? <div className="isbnCard"> 
+							
+							{this.state.book.imageLinks ? <img className="isbnBookImage" src={this.state.book.imageLinks.thumbnail} alt="No image" /> : null}
+							<div className="bookInfo">
+							{this.state.book.title ? <h3 className="text"> {this.state.book.title} </h3> : null}
+							{this.state.book.subtitle ? <h5 className="text"> {this.state.book.subtitle} </h5> : null}
+							{this.state.book.authors ? <h4 className="text"> Authors: {this.state.book.authors.join(', ')} </h4> : null}
+							{this.state.book.description ? <h4 className="description"> Description: {this.state.book.description} </h4> : null}
+							{this.state.book.publisher ? <h4 className="text"> Publisher: {this.state.book.publisher} </h4> : null}
+							{this.state.book.publishedDate ? <h4 className="text"> Publication date: {this.state.book.publishedDate} </h4> : null}
+							</div>
+							
+			</div> : null}
 			
 		</div>
 	
