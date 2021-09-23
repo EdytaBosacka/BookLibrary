0213492d14dcc60ee13eba93f02dcd74de08341f
Edyta <edyta.bosacka@gmail.com>
2021-09-23 20:54:31 +0200

    Adding endpoint to update rating

--- BookLibrary_Server/src/main/java/books/Book.java
+++ BookLibrary_Server/src/main/java/books/Book.java
@@ -1,6 +1,7 @@
 package books;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
@@ -40,5 +41,49 @@ public class Book {
 			return Response.status(Response.Status.NOT_FOUND).entity("File not found").build();
 		}
 	}
+
+	@PUT
+	@Path("/{isbn}/{rating}")
+	public Response updateBookRating(@PathParam("isbn") String bookIsbn, @PathParam("rating") String starRating) {
+		LibraryUtils utils = new LibraryUtils();
+		JSONObject bookJson = utils.readFile();
+
+		if (bookJson != null) {
+			JSONArray bookList = (JSONArray) bookJson.get("items");
+
+			for(int i=0; i< bookList.size(); i++)
+			{	
+				Object book = bookList.get(i);
+				JSONObject bookJsonObject = (JSONObject) book;
+				JSONObject volumeInfo = (JSONObject) (bookJsonObject).get("volumeInfo");
+				JSONArray industryIdentifiers = (JSONArray) volumeInfo.get("industryIdentifiers");
+
+				for (Object industryIdentifier : industryIdentifiers) {
+					String identifier = (String) ((JSONObject) industryIdentifier).get("identifier");
+
+					if (identifier.equals(bookIsbn)) {
+						Double rating = Double.parseDouble( volumeInfo.get("averageRating").toString());
+						Integer ratingsCount = Integer.valueOf( volumeInfo.get("ratingsCount").toString());
+						rating = (rating*ratingsCount + Double.parseDouble(starRating))/(ratingsCount+1);
+						ratingsCount++;
+						volumeInfo.put("averageRating", rating);
+						volumeInfo.put("ratingsCount", ratingsCount);
+						bookJsonObject.put("volumeInfo", volumeInfo);
+						bookList.set(i, bookJsonObject);
+						bookJson.put("items", bookList);
+						utils.writeToFile(bookJson);
+						
+						return Response.status(Response.Status.OK).entity("Updated").build();
+					}
+				}
+			}
+			return Response.status(Response.Status.NOT_FOUND).entity("No results found").build();
+		} else {
+			return Response.status(Response.Status.NOT_FOUND).entity("File not found").build();
+		}
+	}
+	
+	
+	
 
 }
--- BookLibrary_Server/src/main/java/books/LibraryUtils.java
+++ BookLibrary_Server/src/main/java/books/LibraryUtils.java
@@ -1,10 +1,14 @@
 package books;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -32,6 +36,18 @@ public class LibraryUtils {
 		}
 		return null;
 
+	}
+	
+	public void writeToFile(JSONObject json) {
+		FileWriter myWriter;
+		try {
+			File resourcesDirectory = new File("src/main/resources/books2.json");
+			myWriter = new FileWriter(resourcesDirectory.getAbsolutePath());
+			json.writeJSONString(myWriter);
+			myWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
